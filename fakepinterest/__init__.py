e095e3bf37ab9cbf74918cac8553315243943498
kadu13 <k4du.abraao@gmail.com>
2024-06-24 16:38:12 -0300

    mudanca banco postgresql

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -2,10 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+app.config["SQLALCHEMY_DATABASE_URI"] =os.getenv("DATABASE_URL") #"sqlite:///comunidade.db"
 app.config["SECRET_KEY"] = "28d16c04"
 app.config["UPLOAD_FODER"] = "static/fotos_posts"
 
