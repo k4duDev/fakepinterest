fc68c038dc9974dde5bd50a84c5ae18906493739
kadu13 <k4du.abraao@gmail.com>
2024-06-24 17:20:10 -0300

    ajust login

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -6,7 +6,7 @@ import os
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] =os.getenv("DATABASE_URL") #"sqlite:///comunidade.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SECRET_KEY"] = "28d16c04"
 app.config["UPLOAD_FODER"] = "static/fotos_posts"
 
